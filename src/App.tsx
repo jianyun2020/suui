6eb6d905570142b29bd4b737c674950f2f412f97
jianyun2020 <sujianyun2020@163.com>
2022-09-17 10:33:16 +0800

    test menu component

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,18 @@ function App() {
         link3
       </MenuItem>
      </Menu>
+     <br />
+     <Menu mode='vertical'  defaultIndex={0} onSelect={(index) => alert(index)}>
+      <MenuItem index={0}>
+        link1
+      </MenuItem>
+      <MenuItem index={1} disabled>
+        link2
+      </MenuItem>
+      <MenuItem index={2}>
+        link3
+      </MenuItem>
+     </Menu>
     </div>
   );
 }
